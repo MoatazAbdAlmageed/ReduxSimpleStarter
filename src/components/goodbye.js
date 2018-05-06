9480a8283b08d172866f15fbc9b10d8159343987
moataz <moataz.mohammady@gmail.com>
2018-05-06 14:19:25 +0200

    Convert Goodbye component to Stateless component

--- src/components/goodbye.js
+++ src/components/goodbye.js
@@ -1,38 +1,44 @@
-// import React from 'react';
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+// // import React from 'react';
+// import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+//
+//
+// export default class Goodbye extends Component {
+//
+//
+//
+//
+//     constructor(props) {
+//         super(props);
+//         this.state = { clicks: 0 };
+//     }
+//
+//     handleClick() {
+//         this.setState({clicks: ++this.state.clicks});
+//     }
+//
+//
+//     render() {
+//         return (
+//             <h1 onClick={() => this.handleClick()}>Goodbye, {this.props.name} <span >{this.state.clicks} time(s)!</span></h1>
+//
+//
+//         );
+//     }
+// }
+//
+// Goodbye.defaultProps = {
+//     name: 'salah',
+// };
+//
+//
+//
+// Goodbye.propTypes = {
+//     name: PropTypes.string.required,
+// };
 
-
-export default class Goodbye extends Component {
-
-
-
-
-    constructor(props) {
-        super(props);
-        this.state = { clicks: 0 };
-    }
-
-    handleClick() {
-        this.setState({clicks: ++this.state.clicks});
-    }
-
-
-    render() {
-        return (
-            <h1 onClick={() => this.handleClick()}>Goodbye, {this.props.name} <span >{this.state.clicks} time(s)!</span></h1>
-
-
-        );
-    }
-}
-
-Goodbye.defaultProps = {
-    name: 'salah',
-};
-
-
-
-Goodbye.propTypes = {
-    name: PropTypes.string.required,
-};
+// Stateless component
+import React from 'react';
+// Normally the first argument to this function is props, However using a feature in ES6 called destructuring, we can replace ‘props’ with ‘{name}’ which is the equivalent to having ‘const name = props.name’
+const Goodbye = ({name}) => <h1>Goodbye, {name}!</h1>;
+export default Goodbye;
